e80002baa796f5f620dd19f4dcb8de77c0a92eeb
LordEnder_Kitty <dragonlancenerd@gmail.com>
2022-12-17 15:23:26 -0600

    updated to 1.19.3

--- src/main/java/net/enderkitty/netherite/Netherite.java
+++ src/main/java/net/enderkitty/netherite/Netherite.java
@@ -3,6 +3,9 @@ package net.enderkitty.netherite;
 import net.enderkitty.netherite.block.ModBlocks;
 import net.enderkitty.netherite.item.ModItems;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,5 +17,19 @@ public class Netherite implements ModInitializer {
 	public void onInitialize() {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
+
+
+		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> {
+			content.addAfter(Items.NETHERITE_INGOT, ModItems.PLATED_DIAMOND);
+			content.addAfter(ModItems.PLATED_DIAMOND, ModItems.REINFORCED_CRYSTAL);
+			content.addAfter(ModItems.REINFORCED_CRYSTAL, ModItems.CELESTIAL_CRYSTAL);
+			content.addAfter(ModItems.CELESTIAL_CRYSTAL, ModItems.CRACKED_CELESTIAL_CRYSTAL);
+			content.addAfter(ModItems.CRACKED_CELESTIAL_CRYSTAL, ModItems.HARD_CRYSTAL_FRAGMENT);
+			content.addAfter(ModItems.HARD_CRYSTAL_FRAGMENT, ModItems.LIQUIDIZED_END_CRYSTAL);
+			content.addAfter(ModItems.LIQUIDIZED_END_CRYSTAL, ModItems.NETHER_FRAGMENT);
+			content.addAfter(ModItems.NETHER_FRAGMENT, ModItems.NETHERITE_SCRAP_PIECE);
+			content.addAfter(ModItems.NETHERITE_SCRAP_PIECE, ModItems.REINFORCED_CRYSTAL_DUST);
+			content.addAfter(ModItems.REINFORCED_CRYSTAL_DUST, ModBlocks.BLOCK_OF_REINFORCED_CRYSTAL);
+		});
 	}
 }
--- src/main/java/net/enderkitty/netherite/block/ModBlocks.java
+++ src/main/java/net/enderkitty/netherite/block/ModBlocks.java
@@ -4,37 +4,30 @@ import net.enderkitty.netherite.Netherite;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.block.Material;
-import net.minecraft.block.OreBlock;
-import net.minecraft.client.item.TooltipContext;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
-import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.intprovider.UniformIntProvider;
-import net.minecraft.util.registry.Registry;
-import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
-
-import java.util.List;
 
 public class ModBlocks {
 
     public static final Block BLOCK_OF_REINFORCED_CRYSTAL = registerBlock("block_of_reinforced_crystal",
-            new OreBlock(FabricBlockSettings.of(Material.METAL).strength(8.0f).requiresTool()
-                    .sounds(BlockSoundGroup.COPPER), UniformIntProvider.create(1, 10)), ItemGroup.MISC);
+            new ExperienceDroppingBlock(FabricBlockSettings.of(Material.METAL).strength(8.0f).requiresTool()
+                    .sounds(BlockSoundGroup.COPPER), UniformIntProvider.create(1, 10)));
 
-    private static Block registerBlock(String name, Block block, ItemGroup group) {
-        registerBlockItem(name, block, group);
-        return Registry.register(Registry.BLOCK, new Identifier(Netherite.MOD_ID, name), block);
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(Netherite.MOD_ID, name), block);
     }
 
-    private static Item registerBlockItem(String name, Block block, ItemGroup group) {
-        return Registry.register(Registry.ITEM, new Identifier(Netherite.MOD_ID, name),
-                new BlockItem(block, new FabricItemSettings().group(group)));
+    private static Item registerBlockItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(Netherite.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
     }
 
     public static void registerModBlocks() {
