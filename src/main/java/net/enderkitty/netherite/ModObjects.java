50501942c8dd4041825e03bfa84fa710935c29c2
LordEnder_Kitty <dragonlancenerd@gmail.com>
2023-03-14 23:55:20 -0500

    updated to 1.19.4

--- src/main/java/net/enderkitty/netherite/ModObjects.java
+++ src/main/java/net/enderkitty/netherite/ModObjects.java
@@ -1,13 +1,19 @@
-package net.enderkitty.netherite.item;
+package net.enderkitty.netherite;
 
-import net.enderkitty.netherite.Netherite;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
-public class ModItems {
+public class ModObjects {
 
     public static final Item PLATED_DIAMOND = registerItem("plated_diamond",
             new Item(new FabricItemSettings()));
@@ -28,12 +34,24 @@ public class ModItems {
     public static final Item REINFORCED_CRYSTAL_DUST = registerItem("reinforced_crystal_dust",
             new Item(new FabricItemSettings()));
 
+    public static final Block BLOCK_OF_REINFORCED_CRYSTAL = registerBlock("block_of_reinforced_crystal",
+            new ExperienceDroppingBlock(FabricBlockSettings.of(Material.METAL).strength(8.0f).requiresTool()
+                    .sounds(BlockSoundGroup.COPPER), UniformIntProvider.create(1, 10)));
 
+
+    private static Block registerBlock(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(Registries.BLOCK, new Identifier(Netherite.MOD_ID, name), block);
+    }
+    private static Item registerBlockItem(String name, Block block) {
+        return Registry.register(Registries.ITEM, new Identifier(Netherite.MOD_ID, name),
+                new BlockItem(block, new FabricItemSettings()));
+    }
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, new Identifier(Netherite.MOD_ID, name), item);
     }
 
-    public static void registerModItems() {
-        Netherite.LOGGER.debug("Registering Mod Items for " + Netherite.MOD_ID);
+    public static void registerModObjects() {
+        Netherite.LOGGER.debug("Registering blocks/items for Easier Netherite");
     }
 }
--- src/main/java/net/enderkitty/netherite/Netherite.java
+++ src/main/java/net/enderkitty/netherite/Netherite.java
@@ -1,7 +1,5 @@
 package net.enderkitty.netherite;
 
-import net.enderkitty.netherite.block.ModBlocks;
-import net.enderkitty.netherite.item.ModItems;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.ItemGroups;
@@ -15,21 +13,19 @@ public class Netherite implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		ModItems.registerModItems();
-		ModBlocks.registerModBlocks();
-
+		ModObjects.registerModObjects();
 
 		ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(content -> {
-			content.addAfter(Items.NETHERITE_INGOT, ModItems.PLATED_DIAMOND);
-			content.addAfter(ModItems.PLATED_DIAMOND, ModItems.REINFORCED_CRYSTAL);
-			content.addAfter(ModItems.REINFORCED_CRYSTAL, ModItems.CELESTIAL_CRYSTAL);
-			content.addAfter(ModItems.CELESTIAL_CRYSTAL, ModItems.CRACKED_CELESTIAL_CRYSTAL);
-			content.addAfter(ModItems.CRACKED_CELESTIAL_CRYSTAL, ModItems.HARD_CRYSTAL_FRAGMENT);
-			content.addAfter(ModItems.HARD_CRYSTAL_FRAGMENT, ModItems.LIQUIDIZED_END_CRYSTAL);
-			content.addAfter(ModItems.LIQUIDIZED_END_CRYSTAL, ModItems.NETHER_FRAGMENT);
-			content.addAfter(ModItems.NETHER_FRAGMENT, ModItems.NETHERITE_SCRAP_PIECE);
-			content.addAfter(ModItems.NETHERITE_SCRAP_PIECE, ModItems.REINFORCED_CRYSTAL_DUST);
-			content.addAfter(ModItems.REINFORCED_CRYSTAL_DUST, ModBlocks.BLOCK_OF_REINFORCED_CRYSTAL);
+			content.addAfter(Items.NETHERITE_INGOT, ModObjects.PLATED_DIAMOND);
+			content.addAfter(ModObjects.PLATED_DIAMOND, ModObjects.REINFORCED_CRYSTAL);
+			content.addAfter(ModObjects.REINFORCED_CRYSTAL, ModObjects.CELESTIAL_CRYSTAL);
+			content.addAfter(ModObjects.CELESTIAL_CRYSTAL, ModObjects.CRACKED_CELESTIAL_CRYSTAL);
+			content.addAfter(ModObjects.CRACKED_CELESTIAL_CRYSTAL, ModObjects.HARD_CRYSTAL_FRAGMENT);
+			content.addAfter(ModObjects.HARD_CRYSTAL_FRAGMENT, ModObjects.LIQUIDIZED_END_CRYSTAL);
+			content.addAfter(ModObjects.LIQUIDIZED_END_CRYSTAL, ModObjects.NETHER_FRAGMENT);
+			content.addAfter(ModObjects.NETHER_FRAGMENT, ModObjects.NETHERITE_SCRAP_PIECE);
+			content.addAfter(ModObjects.NETHERITE_SCRAP_PIECE, ModObjects.REINFORCED_CRYSTAL_DUST);
+			content.addAfter(ModObjects.REINFORCED_CRYSTAL_DUST, ModObjects.BLOCK_OF_REINFORCED_CRYSTAL);
 		});
 	}
 }
